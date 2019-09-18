1e47118545bbd2d63bceb734744b0834a8cd4256
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-09-19 00:38:22 +0100

    add header to landing page

---
+++ src/components/atoms/Avatar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Avatar() {
+  return (
+    <img
+      alt="avatar"
+      src="https://res.cloudinary.com/iyikuyoro/image/upload/v1550737464/portfolio%20assets/IMG_8579.jpg"
+    />
+  );
+}
---
+++ src/components/compounds/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Avatar from 'Atoms/Avatar';
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Opeoluwa Iyi-Kuyoro</h1>
+      <Avatar />
+    </div>
+  );
+}
