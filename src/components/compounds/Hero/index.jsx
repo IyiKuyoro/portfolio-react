e3bb95f87ae6cdc66349e2bd0b83959b67a5f274
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-09-19 15:44:43 +0100

    add stack image component

--- src/components/compounds/Hero/index.jsx
+++ src/components/compounds/Hero/index.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
 import Styles from './hero.styles';
+import stacks from './StackList.json';
 
 export default class Hero extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentStack: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.hookupShuffling();
+  }
+
+  hookupShuffling() {
+    const maxStack = 15;
+
+    setInterval(() => {
+      const { currentStack } = this.state;
+
+      if (currentStack === maxStack + 1) {
+        this.setState({
+          currentStack: 0,
+        });
+      } else {
+        this.setState({
+          currentStack: currentStack + 1,
+        });
+      }
+    }, 1000);
   }
 
   render() {
+    const { currentStack } = this.state;
+
     return (
       <div className={Styles.hero}>
         <div className={Styles.hero_text}>
@@ -22,6 +49,11 @@ export default class Hero extends Component {
             + ' Scroll down to get to know me a bit better'}
           </p>
         </div>
+        <img
+          className={Styles.langImage}
+          src={stacks[currentStack].imageUrl}
+          alt={stacks[currentStack].name}
+        />
       </div>
     );
   }
