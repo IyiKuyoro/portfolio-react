afd7a5f44bff36022999141195ad2708e6580801
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-09-20 21:46:27 +0100

    add project component

---
+++ src/components/compounds/Projects/Project/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Styles from './project.styles';
+
+export default function Project(props) {
+  const { project, className } = props;
+
+  return (
+    <div className={`${className} ${Styles.project}`}>
+      <img
+        className={Styles.image}
+        src={project.imageUrl}
+        alt={project.name}
+      />
+      <div className={Styles.text}>
+        <h3 className={Styles.name}>{project.name}</h3>
+        <p className={Styles.description}>{project.description}</p>
+      </div>
+    </div>
+  );
+}
+
+Project.propTypes = {
+  className: PropTypes.string.isRequired,
+  project: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/components/compounds/Projects/index.jsx
+++ src/components/compounds/Projects/index.jsx
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
 
+import Project from './Project';
 import Styles from './projects.styles';
+import ProjectList from './ProjectsList.json';
 
 export default class Projects extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentId: 0,
+      projectClassList: [],
+    };
+  }
+
+  componentDidMount() {
+    // this.removeClassAtIntervals();
+    this.changeProjectAtIntervals();
+  }
+
+  changeProjectAtIntervals() {
+    setInterval(() => {
+      // Pull project off screen
+      this.setState({
+        projectClassList: [Styles.removeProject],
+      });
+
+      // Change Project
+      setTimeout(() => {
+        const { currentId } = this.state;
+
+        if (currentId === ProjectList.length - 1) {
+          this.setState({
+            currentId: 0,
+          });
+        } else {
+          this.setState({
+            currentId: currentId + 1,
+          });
+        }
+      }, 500);
+
+      // Bring project back
+      setTimeout(() => {
+        this.setState({
+          projectClassList: [Styles.addProject],
+        });
+      }, 1000);
+    }, 6000);
   }
 
   render() {
+    const { currentId, projectClassList } = this.state;
+    const projectClassString = projectClassList.join(' ');
+
     return (
       <div className={Styles.projects}>
         <button className={Styles.btn} type="button"> </button>
         <h2 className={Styles.heading}>See some of my open source projects</h2>
+        <Project className={projectClassString} project={ProjectList[currentId]} />
         <button className={`${Styles.btn} ${Styles.btnRight}`} type="button"> </button>
+        <a href="https://github.com/IyiKuyoro" className={Styles.link}>...psst there is more on GitHub</a>
       </div>
     );
   }
