ccf47f93ff9f6013a579dae0d5f4c21bb8f8b47b
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-09-22 14:43:18 +0100

    add support next project

--- src/components/compounds/Projects/index.jsx
+++ src/components/compounds/Projects/index.jsx
@@ -7,6 +7,7 @@ import ProjectList from './ProjectsList.json';
 export default class Projects extends Component {
   constructor(props) {
     super(props);
+    this.projectInterval = null;
     this.state = {
       currentId: 0,
       projectClassList: [],
@@ -14,29 +15,24 @@ export default class Projects extends Component {
   }
 
   componentDidMount() {
-    // this.removeClassAtIntervals();
     this.changeProjectAtIntervals();
   }
 
+  // Hooks up the changing of projects at a 6 secs interval
   changeProjectAtIntervals() {
-    setInterval(() => {
-      // Pull project off screen
-      this.slideProjectOut();
-
-      // Change Project
-      this.changeProject();
-
-      // Bring project back
-      this.slideProjectIn();
+    this.projectInterval = setInterval(() => {
+      this.nextProject();
     }, 6000);
   }
 
+  // Slides the old project out of the screen
   slideProjectOut() {
     this.setState({
       projectClassList: [Styles.removeProject],
     });
   }
 
+  // Slides a new project onto the screen
   slideProjectIn() {
     setTimeout(() => {
       this.setState({
@@ -45,6 +41,7 @@ export default class Projects extends Component {
     }, 1000);
   }
 
+  // Change the displayed project
   changeProject() {
     setTimeout(() => {
       const { currentId } = this.state;
@@ -60,6 +57,28 @@ export default class Projects extends Component {
     }, 500);
   }
 
+  // previousProject() {
+  // }
+
+  nextProject() {
+    // Pull project off screen
+    this.slideProjectOut();
+
+    // Change Project
+    this.changeProject();
+
+    // Bring project back
+    this.slideProjectIn();
+  }
+
+  // Reset the project switching interval
+  resetInterval() {
+    clearInterval(this.projectInterval);
+    this.projectInterval = setInterval(() => {
+      this.nextProject();
+    }, 6000);
+  }
+
   render() {
     const { currentId, projectClassList } = this.state;
     const projectClassString = projectClassList.join(' ');
@@ -69,7 +88,16 @@ export default class Projects extends Component {
         <button className={Styles.btn} type="button"> </button>
         <h2 className={Styles.heading}>See some of my open source projects</h2>
         <Project className={projectClassString} project={ProjectList[currentId]} />
-        <button className={`${Styles.btn} ${Styles.btnRight}`} type="button"> </button>
+        <button
+          className={`${Styles.btn} ${Styles.btnRight}`}
+          onClick={() => {
+            this.nextProject();
+            this.resetInterval();
+          }}
+          type="button"
+        >
+          {}
+        </button>
         <a href="https://github.com/IyiKuyoro" className={Styles.link}>...psst there is more on GitHub</a>
       </div>
     );
