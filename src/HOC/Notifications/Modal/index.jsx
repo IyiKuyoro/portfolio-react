1bb42f18d4d36350bc19969ff8e3f23cb6b3bbdb
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-11-08 16:36:09 +0100

    feat(notifications): style modal notification
    - refactor modal notification display

--- src/HOC/Notifications/Modal/index.jsx
+++ src/HOC/Notifications/Modal/index.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { string } from 'prop-types';
 
-export default function ModalNotification() {
-  return (
-    <div>Modal Notification Works</div>
-  );
+import Styles from './modalNotifications.styles.scss';
+
+export default class ModalNotification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  render() {
+    const { message, severity } = this.props;
+    const { open } = this.state;
+    let style = Styles.modal;
+
+    switch (severity) {
+      case 'info':
+        style += ` ${Styles.info}`;
+        break;
+      case 'caution':
+        style += ` ${Styles.caution}`;
+        break;
+      default:
+        style += ` ${Styles.error}`;
+        break;
+    }
+
+    return (
+      <div
+        tabIndex="0"
+        role="button"
+        onKeyDown={(e) => {
+          if (e.keyCode === 27) this.handleClick();
+        }}
+        onClick={this.handleClick}
+        className={open ? Styles.overlay : Styles.noOverlay}
+      >
+        <div className={style}>
+          <p className={Styles.message}>{message}</p>
+          <button
+            onClick={this.handleClick}
+            type="button"
+            className={Styles.closeButton}
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
+ModalNotification.propTypes = {
+  message: string.isRequired,
+  severity: string.isRequired,
+};
