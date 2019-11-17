339ea3bff4b61ee95a0a88fd0b19419d0a7eb619
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-11-17 17:34:24 +0100

    feat(article): add article edit button

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -3,21 +3,29 @@ import PropTypes from 'prop-types';
 import TimePast from 'time_past';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import BallonEditor from '@ckeditor/ckeditor5-build-balloon';
+import { connect } from 'react-redux';
 
+import Button from 'Atoms/Button';
 import Header from 'Compounds/Header';
 import Disqus from 'Compounds/Disqus';
 import Styles from './article.styles.scss';
+
+function canEdit(authorsIds, id) {
+  return authorsIds.indexOf(id) >= 0;
+}
 
 function Article(props) {
   const {
     article: {
       title,
       authors,
+      authorsIds,
       updatedAt,
       body,
       imageUrl,
       slug,
     },
+    userData: { id },
   } = props;
 
   const authorsString = authors.reduce((acc, name, i) => {
@@ -37,6 +45,7 @@ function Article(props) {
   return (
     <div>
       <Header />
+      {canEdit(authorsIds, id) && <Button style={Styles.btn} text="Edit" />}
       {imageUrl && <img className={Styles.image} src={imageUrl} alt="article banner" />}
       <h2 className={Styles.title}>{title}</h2>
       <p className={Styles.authors}>{authorsString}</p>
@@ -53,16 +62,32 @@ function Article(props) {
   );
 }
 
+Article.defaultProps = {
+  userData: {
+    id: -1,
+  },
+};
+
 Article.propTypes = {
   article: PropTypes.shape({
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
     authors: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+    authorsIds: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
     imageUrl: PropTypes.string,
     updatedAt: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
   }).isRequired,
+  userData: PropTypes.shape({
+    id: PropTypes.number,
+  }),
 };
 
-export default Article;
+function mapStateToProps(state) {
+  return ({
+    userData: state.authUser.userData,
+  });
+}
+
+export default connect(mapStateToProps)(Article);
