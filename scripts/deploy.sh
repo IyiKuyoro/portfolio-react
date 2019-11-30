088d60cd0d27c2cb4fca93cd3e57e489a2902aaa
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-11-30 21:21:33 +0100

    modify config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 # This script deploys our application to our digital ocean droplet
 
-ssh ${::env(user)}@${::env(dropletIP)}
+ssh $user@$ropletIP}
 git clone git@github.com:IyiKuyoro/portfolio-react.git
 cd portfolio-react
 npm install
@@ -10,7 +10,7 @@ npm run build
 cd ~
 sudo rm /var/www/html/*
 expect {
-  "[sudo] password for $user*" {send -- "${::env(DOPLET_USER_PASSWORD)}\r"}
+  "[sudo] password for $user*" {send -- "$DOPLET_USER_PASSWORD\r"}
 }
 expect eof
 sudo mv -v portfolio-react/dist/* /var/www/html
